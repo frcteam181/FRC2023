be516a4c17ff8b236b76bb04377d521c7b4bad57
Paulo Silva <pangerspol@gmail.com>
2023-01-27 14:20:04 -0500

    Fixed gains
    fixed variables names

--- src/main/java/frc/robot/Gains.java
+++ src/main/java/frc/robot/Gains.java
@@ -5,18 +5,20 @@ public class Gains {
 	public final double kP;
 	public final double kI;
 	public final double kD;
-	public final double kF;
+	public final double kFF;
 	public final int kIzone;
-	public final double kPeakOutput;
+	public final double kMinOutput;
+	public final double kMaxOutput;
 
-	public Gains(double _kP, double _kI, double _kD, double _kF, int _kIzone, double _kPeakOutput) {
+	public Gains(double _kP, double _kI, double _kD, double _kFF, int _kIzone, double _kMinOutput, double _kMaxOutput) {
 
 		kP = _kP;
 		kI = _kI;
 		kD = _kD;
-		kF = _kF;
+		kFF = _kFF;
 		kIzone = _kIzone;
-		kPeakOutput = _kPeakOutput;
+		kMinOutput = _kMinOutput;
+		kMaxOutput = _kMaxOutput;
 
 	}
 }
