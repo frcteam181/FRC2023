cffc89a6fb849a5cc89aa59f2b4904e18145dc15
Paulo Silva <pangerspol@gmail.com>
2023-01-28 01:38:51 -0500

    Updated RoboContainer
    Initiated arm subsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,16 +2,20 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.controllers.DriverController;
+import frc.robot.controllers.OperatorController;
 import frc.robot.shuffleboard.CompetitionTab;
+import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.DriveBase;
 
 public class RobotContainer {
 
   //Subsystems
   private DriveBase m_driveBase;
+  private Arm m_arm;
 
   //Controllers
   private DriverController m_driverController;
+  private OperatorController m_operatorController;
   
   //Shuffleboard Tabs
   private final CompetitionTab m_competitionTab;
@@ -20,12 +24,14 @@ public class RobotContainer {
 
     //Subsystems
     m_driveBase = new DriveBase();
+    m_arm = new Arm();
 
     //Controllers
     m_driverController = new DriverController(m_driveBase);
+    m_operatorController = new OperatorController(m_arm);
 
     //Shuffleboard Tabs
-    m_competitionTab = new CompetitionTab(m_driveBase);
+    m_competitionTab = new CompetitionTab(m_driveBase, m_arm);
   
   }
 
