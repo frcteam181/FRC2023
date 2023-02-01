82b3d4b3e4141f3419818fc5c907e0d02055ab7a
Paulo Silva <pangerspol@gmail.com>
2023-01-31 19:16:18 -0500

    Do nothing auto
    Lets agree to never use this

---
+++ src/main/java/frc/robot/commands/auto_commands/doNothing.java
@@ -0,0 +1,13 @@
+package frc.robot.commands.auto_commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class doNothing extends CommandBase {
+
+    public doNothing() {
+
+        addRequirements();
+
+    }
+    
+}
