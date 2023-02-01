e9f8e664543a7dd8b8c841a4f1c759c47c60a3c2
Paulo Silva <pangerspol@gmail.com>
2023-01-31 19:21:57 -0500

    Relative to Absolute Encoders

--- src/main/java/frc/robot/shuffleboard/CompetitionTab.java
+++ src/main/java/frc/robot/shuffleboard/CompetitionTab.java
@@ -1,9 +1,13 @@
 package frc.robot.shuffleboard;
 
+import java.util.Map;
+
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.auto_commands.doNothing;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.DriveBase;
 
@@ -26,22 +30,31 @@ public class CompetitionTab {
         m_driveBase = driveBase;
         m_arm = arm;
 
-        configureAuto();
+        configureAuto(m_autoChooser, m_competitionTab);
 
         /* (Column , Row) starting from index #0  15x7 */
-        m_competitionTab.add("Field", m_driveBase.getField()).withPosition(8, 0).withSize(7, 4);
-
+        m_competitionTab.add("Field", m_driveBase.getField()).withPosition(7, 0).withSize(7, 4);
+        m_competitionTab.addDouble("Stored Pressure", m_driveBase::getStoredPSI).withWidget(BuiltInWidgets.kDial).withProperties(Map.of("min", 0, "max", 120)).withPosition(9, 4).withSize(2, 2);
+        m_competitionTab.addDouble("Working Pressure", m_driveBase::getWorkingPSI).withWidget(BuiltInWidgets.kDial).withProperties(Map.of("min", 0, "max", 60)).withPosition(11, 4).withSize(2, 2);
+        m_competitionTab.addBoolean("Compressor Satus", m_driveBase::getCompressorStatus).withPosition(4, 5).withSize(3, 1);
+        //m_competitionTab.add("Pigeon 2", m_driveBase.getYaw()).withWidget(BuiltInWidgets.kGyro).withPosition(7, 4).withSize(2, 2);
+        //m_competitionTab.add("Left Encoder", m_driveBase.getLeftEncoder()).withWidget(BuiltInWidgets.kEncoder).withPosition(0, 5).withSize(2, 2);
+        //m_competitionTab.add("Right Encoder", m_driveBase.getRightEncoder()).withWidget(BuiltInWidgets.kEncoder).withPosition(2, 5).withSize(2, 2);
+        //m_competitionTab.add("Power Distribution Hub", m_driveBase.getPDH()).withWidget(BuiltInWidgets.kPowerDistribution).withPosition(4, 0).withSize(3, 4);
+        m_competitionTab.addDouble("Channel 7 (amps)", m_driveBase::getChannel7).withPosition(7, 4).withSize(1, 1);
+        m_competitionTab.addDouble("Channel 12 (amps)", m_driveBase::getChannel12).withPosition(8, 4).withSize(1, 1);
     }
+    
 
-    public void configureAuto(/* May need to pass objects here. Meybe it'll be better to not do this method at all `\'-'/` */) {
+    public void configureAuto(SendableChooser chooser, ShuffleboardTab tab) {
 
         //Set Default Auto
-        //m_autoChooser.setDefaultOption("Taxi Only", new TaxiOnly(m_driveTrain));
+        chooser.setDefaultOption("Do Nothing", new doNothing());
 
         // Add auto options here:
-        //m_autoChooser.addOption("Taxi Only", new TaxiOnly(m_driveTrain));
+        chooser.addOption("Do Nothing", new doNothing());
 
-        //m_competitionTab.add("Choose Auto", m_autoChooser).withWidget(BuiltInWidgets.kComboBoxChooser).withPosition(4, 3).withSize(2, 1);
+        tab.add("Choose Auto", chooser).withWidget(BuiltInWidgets.kComboBoxChooser).withPosition(4, 4).withSize(3, 1);
 
     }
 
