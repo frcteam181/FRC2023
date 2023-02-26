35460c54ba07aa2b2fb08176e896a4e013402820
Paulo Silva <pangerspol@gmail.com>
2023-02-25 21:32:13 -0500

    Drive Base needs tuning

--- src/main/java/frc/robot/controllers/DriverController.java
+++ src/main/java/frc/robot/controllers/DriverController.java
@@ -1,9 +1,7 @@
 package frc.robot.controllers;
 
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.Joystick.ButtonType;
-import edu.wpi.first.wpilibj.PS4Controller.Button;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.POVButton;
 import frc.robot.commands.default_commands.DriveBaseDefaultCommand;
