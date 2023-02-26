e16ed0262eda32787d7b378c12c85ec1f5e0b1a1
Paulo Silva <pangerspol@gmail.com>
2023-02-25 21:33:06 -0500

    Added Trajectory

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,6 +1,35 @@
 package frc.robot;
 
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.util.Units;
+
 public final class Constants {
+
+    /* Physical Robot Variables */
+
+    public static final double k_wheelDiameterMeters = Units.inchesToMeters(6);
+    public static final double k_trackWidthMeters = Units.inchesToMeters(22.65);
+    public static final DifferentialDriveKinematics k_driveKinematics = new DifferentialDriveKinematics(k_trackWidthMeters);
+    public static final double k_robotWeightKillograms = Units.lbsToKilograms(100);
+    public static final double k_robotLengthMeters = Units.inchesToMeters(32.375);
+    public static final double k_robotWidthMeters = Units.inchesToMeters(27.75);
+    public static final double k_robotMomentOfInnertia = k_robotLengthMeters * k_robotWidthMeters * k_robotWeightKillograms * (1/6);
+    public static final double k_gearRatio = 1;
+    public static final double k_maxRPM = 5676;
+    //public static final LinearSystem<N2,N2,N2> k_botPlant = LinearSystemId.createDrivetrainVelocitySystem(DCMotor.getNEO(2), k_robotWeightKillograms, k_wheelDiameterMeters/2, k_trackWidthMeters/2, k_robotMomentOfInnertia, k_gearRatio);
+
+    /* Reference Robot Variables */
+
+    public static final double k_refWheelDiameterMeters = Units.inchesToMeters(6);
+    public static final double k_refTrackWidthMeters = 0.57531; // Provided by SysId
+    public static final DifferentialDriveKinematics k_refDriveKinematics = new DifferentialDriveKinematics(k_trackWidthMeters);
+    public static final double k_refRobotWeightKillograms = Units.lbsToKilograms(100);
+    public static final double k_refRobotLengthMeters = Units.inchesToMeters(32.375);
+    public static final double k_refRobotWidthMeters = Units.inchesToMeters(27.75);
+    public static final double k_refRobotMomentOfInnertia = k_refRobotLengthMeters * k_refRobotWidthMeters * k_refRobotWeightKillograms * (1/6);
+    public static final double k_refGearRatio = 1;
+    public static final double k_refMaxRPM = 5676;
+    //public static final LinearSystem<N2,N2,N2> k_refPlant = LinearSystemId.createDrivetrainVelocitySystem(DCMotor.getNEO(2), k_refRobotWeightKillograms, k_refWheelDiameterMeters/2, k_refTrackWidthMeters/2, k_refRobotMomentOfInnertia, k_refGearRatio);
 
     /* Drive Base Gains */
 
@@ -9,14 +38,63 @@ public final class Constants {
     public static final Gains k_DriveGains = new Gains(0, 0, 0, 0, 0, 0, 0);
     public static final Gains k_TurnGains = new Gains(0, 0, 0, 0, 0, 0, 0);
 
+    // SysId variables for drivebase
+    public static final double k_FFKs = 1.069; // Volts
+    public static final double k_FFKv = 2.9807; // (Volts * Seconds) / Meter
+    public static final double k_FFKa = 0.93873; // (Volts * Seconds^2) / Meter
+    public static final double k_kPDriveVel = 3.1519;
+    public static final double k_AngularKv = 0.93873;
+    public static final double k_AngularKa = 0.93873;
+    public static final double k_MaxSpeedMetersPerSecond = 3;
+    public static final double k_MaxAccelerationMetersPerSecondSquared = 1;
+
+    // Reasonable baseline values for a RAMSETE follower in units of meters and seconds
+    public static final double kRamseteB = 2;
+    public static final double kRamseteZeta = 0.7;
+
     /* Arm Gains */
 
+    // Pivot
     public static final int k_PIVOT_SLOT_ID = 0;
+    public static final double k_pivotKG_m_Gain = 0;
+    public static final double k_pivotKG_b_Gain = 1;
+    public static final double k_pivotOffset = Units.degreesToRadians(-62.6);
+    public static final double k_maxPivotVel = 0.2;
+    public static final double k_maxPivotAcc = 0.5;
+    public static final double k_pivotPosFac = Units.degreesToRadians(4.5);
+    public static final double k_pivotVelFac = 1;
+    public static final double k_pivotKs = 0;
+    public static final double k_pivotKg = 6;
+    public static final double k_pivotKv = 0;
+    public static final double k_pivotKa = 0;
+    public static final Gains k_PivotGains = new Gains(0, 0, 0, 0, 0, -1, 1);
+
+    // Spool
     public static final int k_SPOOL_SLOT_ID = 0;
-    public static final double k_ArmKG_m_Gain = 0.1;
-    public static final double k_ArmKG_b_Gain = 0;
-    public static final Gains k_PivotGains = new Gains(0, 0, 0, 0, 0, 0, 0);
-    public static final Gains k_SpoolGains = new Gains(0, 0, 0, 0, 0, 0, 0);
+    public static final double k_spoolPosFac = 1;
+    public static final Gains k_SpoolGains = new Gains(0, 0, 0, 0, 0, -1, 1);
+
+    // Claw
+
+    // Beams
+
+    /* Turret Gains */
+
+    public static final int k_TURRET_SLOT_ID = 0;
+    public static final Gains k_TurretGains = new Gains(0, 0, 0, 0, 0, -1, 1);
+    public static final double k_maxTurretVel = 3;
+    public static final double k_maxTurretAcc = 1.5;
+    public static final double k_turretOffset = 0;
+    public static final double k_turretPosFac = Units.degreesToRadians(0.77922077922);
+    public static final double k_turretVelFac = Units.degreesToRadians(1);
+    public static final double k_turretKs = 0;
+    public static final double k_turretKv = 0;
+    public static final double k_turretKa = 0;
+
+    /* Intake Gains */
+
+    public static final int k_INTAKE_SLOT_ID = 0;
+    public static final Gains k_IntakeGains = new Gains(0.11, 0, 0, 0, 0, 0, 0);
 
     /* Controller Ports */
 
@@ -47,19 +125,29 @@ public final class Constants {
     public static final int k_RIGHT_FOLLOWER = 5;
     public static final int k_RIGHT_LEADER = 6;
     public static final int k_PIGEON = 7;
-    public static final int k_LEFT_PIVOT = 8;
-    public static final int k_RIGHT_PIVOT = 9;
+    public static final int k_RIGHT_PIVOT = 8;
+    public static final int k_LEFT_PIVOT = 9;
     public static final int k_SPOOL = 10;
+    public static final int k_TALON_PIVOT = 11;
+    public static final int k_TURRET = 12;
+    public static final int k_LEFT_INTAKE = 13;
+    public static final int k_RIGHT_INTAKE = 14;
+
+    /* Digital Input Output IDs */
+
+    public static final int k_PIVOT_HOME_BEAM_ID = 0;
+    public static final int k_PIVOT_ZERO_BEAM_ID = 1;
+    public static final int k_SPOOL_HOME_BEAM_ID = 2;
+    public static final int k_SPOOL_MAX_BEAM_ID = 3;
 
     /* Pneumatics Ports */
 
     public static final int k_STORED_PSI = 0;
     public static final int k_WORKING_PSI = 1;
-    public static final int k_INTAKE_PISTON = 0;
+    //public static final int k_INTAKE_PISTON = 0; //Not used may delete soon
+    public static final int k_CLAW_GRAB = 0;
+    public static final int k_CLAW_RELEASE = 1;
 
     // PDH Ports
-
-    public static final int k_PDH_CHANNEL7 = 7;
-    public static final int k_PDH_CHANNEL12 = 12;
 
 }
--- src/main/java/frc/robot/shuffleboard/CompetitionTab.java
+++ src/main/java/frc/robot/shuffleboard/CompetitionTab.java
@@ -1,15 +1,25 @@
 package frc.robot.shuffleboard;
 
+import java.util.List;
 import java.util.Map;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.trajectory.constraint.DifferentialDriveKinematicsConstraint;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import frc.robot.commands.auto_commands.doNothing;
-import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.DriveBase;
+
+import static frc.robot.Constants.*;
 
 public class CompetitionTab {
 
@@ -17,9 +27,8 @@ public class CompetitionTab {
     private SendableChooser<Command> m_autoChooser;
 
     private DriveBase m_driveBase;
-    private Arm m_arm;
     
-    public CompetitionTab(DriveBase driveBase, Arm arm) {
+    public CompetitionTab(DriveBase driveBase) {
 
         ShuffleboardTab m_competitionTab = Shuffleboard.getTab("Competition");
 
@@ -28,38 +37,40 @@ public class CompetitionTab {
 
         //Used Subsystems
         m_driveBase = driveBase;
-        m_arm = arm;
 
-        configureAuto(m_autoChooser, m_competitionTab);
+        /* Set Default Auto */
+        m_autoChooser.setDefaultOption("Do Nothing", new doNothing());
+        // Add auto options here:
+        m_autoChooser.addOption("Do Nothing", new doNothing());
+        m_competitionTab.add("Choose Auto", m_autoChooser).withWidget(BuiltInWidgets.kComboBoxChooser).withPosition(4, 4).withSize(3, 1);
 
         /* (Column , Row) starting from index #0  15x7 */
-        m_competitionTab.add("Field", m_driveBase.getField()).withPosition(7, 0).withSize(7, 4);
+        m_competitionTab.add("Field", m_driveBase.getField()).withPosition(0, 0).withSize(7, 4);
         m_competitionTab.addDouble("Stored Pressure", m_driveBase::getStoredPSI).withWidget(BuiltInWidgets.kDial).withProperties(Map.of("min", 0, "max", 120)).withPosition(9, 4).withSize(2, 2);
         m_competitionTab.addDouble("Working Pressure", m_driveBase::getWorkingPSI).withWidget(BuiltInWidgets.kDial).withProperties(Map.of("min", 0, "max", 60)).withPosition(11, 4).withSize(2, 2);
         m_competitionTab.addBoolean("Compressor Satus", m_driveBase::getCompressorStatus).withPosition(4, 5).withSize(3, 1);
-        //m_competitionTab.add("Pigeon 2", m_driveBase.getYaw()).withWidget(BuiltInWidgets.kGyro).withPosition(7, 4).withSize(2, 2);
-        //m_competitionTab.add("Left Encoder", m_driveBase.getLeftEncoder()).withWidget(BuiltInWidgets.kEncoder).withPosition(0, 5).withSize(2, 2);
-        //m_competitionTab.add("Right Encoder", m_driveBase.getRightEncoder()).withWidget(BuiltInWidgets.kEncoder).withPosition(2, 5).withSize(2, 2);
-        //m_competitionTab.add("Power Distribution Hub", m_driveBase.getPDH()).withWidget(BuiltInWidgets.kPowerDistribution).withPosition(4, 0).withSize(3, 4);
-        m_competitionTab.addDouble("Channel 7 (amps)", m_driveBase::getChannel7).withPosition(7, 4).withSize(1, 1);
-        m_competitionTab.addDouble("Channel 12 (amps)", m_driveBase::getChannel12).withPosition(8, 4).withSize(1, 1);
-    }
-    
-
-    public void configureAuto(SendableChooser chooser, ShuffleboardTab tab) {
-
-        //Set Default Auto
-        chooser.setDefaultOption("Do Nothing", new doNothing());
-
-        // Add auto options here:
-        chooser.addOption("Do Nothing", new doNothing());
-
-        tab.add("Choose Auto", chooser).withWidget(BuiltInWidgets.kComboBoxChooser).withPosition(4, 4).withSize(3, 1);
 
     }
 
     public Command getAuto() {
         return m_autoChooser.getSelected();
+    }
+
+    public Command getAdvancedAuto() {
+
+        // Create config for trajectory
+        TrajectoryConfig config = new TrajectoryConfig(k_MaxSpeedMetersPerSecond, k_MaxAccelerationMetersPerSecondSquared).setKinematics(k_driveKinematics).addConstraint(new DifferentialDriveKinematicsConstraint(k_driveKinematics, 2));
+
+        // An example trajectory to follow.  All units in meters.
+        Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(new Pose2d(0, 0, new Rotation2d(0)), List.of(new Translation2d(1, 1), new Translation2d(2, -1)), new Pose2d(3, 0, new Rotation2d(0)),config);
+
+        RamseteCommand ramseteCommand = new RamseteCommand(exampleTrajectory, m_driveBase::getPose, m_driveBase.getRAMController(), k_driveKinematics, m_driveBase::followPath, m_driveBase);
+
+        // Reset odometry to the starting pose of the trajectory.
+        m_driveBase.resetOdometry(exampleTrajectory.getInitialPose());
+
+        // Run path following command, then stop at the end.
+        return ramseteCommand.andThen(() -> m_driveBase.teleopDrive(0, 0));
     }
 
 }
