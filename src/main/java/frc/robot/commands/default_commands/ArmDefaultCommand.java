b163ca261bb6d2b8b7c72266e79f8e264bc2a823
Paulo Silva <pangerspol@gmail.com>
2023-03-07 20:54:56 -0500

    Update Arm default
    removes free move (not needed)

--- src/main/java/frc/robot/commands/default_commands/ArmDefaultCommand.java
+++ src/main/java/frc/robot/commands/default_commands/ArmDefaultCommand.java
@@ -3,21 +3,24 @@ package frc.robot.commands.default_commands;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Claw;
 import frc.robot.subsystems.Turret;
 
 public class ArmDefaultCommand extends CommandBase{
 
-    private final Arm m_arm;
+    private Arm m_arm;
     private Turret m_turret;
+    private Claw m_claw;
     private final XboxController m_operatorController;
 
-    public ArmDefaultCommand(Arm arm, Turret turret, XboxController operatorController) {
+    public ArmDefaultCommand(Arm arm, Turret turret, Claw claw, XboxController operatorController) {
         
         m_arm = arm;
         m_turret = turret;
+        m_claw = claw;
         m_operatorController = operatorController;
 
-        addRequirements(m_arm, m_turret);
+        addRequirements(m_arm, m_turret, m_claw);
     }
 
     @Override
